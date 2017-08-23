f8fea507e6aed807142d29cf6057a0a0f65e2bec
Noval Agung <caknopal@gmail.com>
2017-08-23 13:42:35 +0000

    update

--- restart.sh
+++ restart.sh
@@ -1,10 +1,10 @@
 sudo kill $(fuser -n tcp fuser -n tcp 2368 2> /dev/null)
 
 log_dirpath="/var/log/ghost/"
-log_filename="log-$(date +%Y-%m-%d:%H:%M:%S).txt"
+log_filename="log-$(date +%Y-%m-%d-%H-%M-%S).txt"
 log_filepath="${log_dirpath}${log_filename}"
 mkdir -p "$log_dirpath"
+echo "log created at $log_filepath"
 touch "$log_filepath"
 
 nohup npm start --production > "$log_filepath" &
-tail -f "$log_filepath"
