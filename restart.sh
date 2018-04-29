0351ce45d28a67f6034d48eafbe722e42e78416c
Noval Agung <caknopal@gmail.com>
2018-04-29 18:59:48 +0000

    backup

--- restart.sh
+++ restart.sh
@@ -1,10 +1,8 @@
-sudo kill $(fuser -n tcp fuser -n tcp 2368 2> /dev/null)
-
+pid=$(lsof -i:3000 -t); kill -TERM $pid || kill -KILL $pid
 log_dirpath="/var/log/ghost/"
 log_filename="log-$(date +%Y-%m-%d-%H-%M-%S).txt"
 log_filepath="${log_dirpath}${log_filename}"
 mkdir -p "$log_dirpath"
 echo "log created at $log_filepath"
 touch "$log_filepath"
-
 nohup npm start --production > "$log_filepath" &
